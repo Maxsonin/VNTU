cb877c6a29df88b08d2b5d13b061f044e80a82db
Maxsonin <maximles.lv@gmail.com>
2023-09-10 19:38:58 +0300

    Revert "Revert "Updated Lab 1 and 2""
    This reverts commit dfc3e6962d937b36ab2440a86b30d18c0b242d2d.

--- Labs/Lab_1_87/Lab_1_87.cpp
+++ Labs/Lab_1_87/Lab_1_87.cpp
@@ -1,18 +1,19 @@
-#include <iostream>
+﻿// Завдання 87. Передбачити введення кута а в градусах i переклад його в радiани
+#define _USE_MATH_DEFINES // Додає макрос M_PI щоб використовувати Pi
+#include <iostream> // cout/cin
+#include <cmath> // Додаємо макрови _USE_MATH_DEFINES
 
-int AskUserForChoise();
+int AskUserForChoise(); // Попереднє оголошення функцiй
 void DegreesToRadiansFunc();
 void RadiansToDegreesFunc();
 
-const double PI = 3.141592653589793238462643383279502884197;
-
 int main()
 {
-    AskUserForChoise() == 1 ? DegreesToRadiansFunc() : RadiansToDegreesFunc();
+    AskUserForChoise() == 1 ? DegreesToRadiansFunc() : RadiansToDegreesFunc(); // Тернарний оператор для зменшення обсягу коду в main
     return 0;
 }
 
-int AskUserForChoise()
+int AskUserForChoise() // Iмплементацiя функцiї яка дубе повертати вибiр користуача
 {
     std::cout << "Choose Type of Conversion:\n"
         << "From Degrees to Radians(press '1')\n"
@@ -21,7 +22,7 @@ int AskUserForChoise()
     int userChoice;
     std::cin >> userChoice;
 
-    if (userChoice != 1 && userChoice != 2)
+    if (userChoice != 1 && userChoice != 2) // Якщо користувач введе невiрне число: вивисти повiдомлення про це та запутстити AskUserForChoise() знову
     {
         std::cout << "Invalid Value! Type '1' or '2'\n\n";
         userChoice = 0;
@@ -33,21 +34,21 @@ int AskUserForChoise()
     }
 }
 
-void DegreesToRadiansFunc()
+void DegreesToRadiansFunc() // Функцiя перетворення з градусiв в радiани
 {
     std::cout << "Type Degrees: ";
     double DegreesVal;
     std::cin >> DegreesVal;
 
-    std::cout << DegreesVal << " Degrees equals " << DegreesVal * PI / 180 << " Radians\n";
+    std::cout << DegreesVal << " Degrees equals " << DegreesVal * M_PI / 180 << " Radians\n";
 }
 
 
-void RadiansToDegreesFunc()
+void RadiansToDegreesFunc() // Функцiя перетворення з радiан в градуси
 {
     std::cout << "Type Radians: ";
     double RadiansVal;
     std::cin >> RadiansVal;
 
-    std::cout << RadiansVal << " Radians equals " << RadiansVal * 180 / PI << " Degrees\n";
+    std::cout << RadiansVal << " Radians equals " << RadiansVal * 180 / M_PI << " Degrees\n";
 }
