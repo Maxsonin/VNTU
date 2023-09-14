bb3151574b4628ff0cb77a89822c301e50ee3d8a
Maxsonin <maximles.lv@gmail.com>
2023-09-14 16:54:22 +0300

    Upgarded Lab_7_63

--- Labs/Lab_7_63/Lab_7_63.cpp
+++ Labs/Lab_7_63/Lab_7_63.cpp
@@ -1,29 +1,52 @@
 ﻿#include <iostream>
 #include <string>
 
+bool IsAlphabetic(std::string string);
+std::string AskAndChackIsDigit();
+
 int main()
+{
+	std::string s = AskAndChackIsDigit();
+
+	if (IsAlphabetic(s))
+		std::cout << "String in alphabetical sequence";
+	else
+		std::cout << "String in NOT in alphabetical sequence";
+
+	return 0;
+}
+
+bool IsAlphabetic(std::string string)
+{
+	/*Якщо число UNICODE currant елемента НЕ дорівнює
+	  наступному числу UNICODE + 1 то це не алфавітний
+	  порядок (A = 65, B = 66)*/
+	for (int i = 0; i < string.length() - 1; i++)
+	{
+		if (tolower(string[i]) + 1 != tolower(string[i + 1]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+std::string AskAndChackIsDigit()
 {
 	std::cout << "Enter string to chack: ";
 	std::string s;
 	std::cin >> s;
-
-	bool IsAlphabetic = true; 
-
-	for (int i = 0; i < s.length() - 1; i++)
+			
+	for (char c : s)
 	{
-		if (tolower(s[i]) + 1 != tolower(s[i + 1])) /*Якщо число UNICODE currant елемента НЕ
-												      дорівнює наступному числу UNICODE + 1 
-													  то це не алфавітний порядок (A = 65, B = 66)*/
+		if (std::isdigit(c))
 		{
-			IsAlphabetic = false;
-			break;
+			std::cout << "Type String WITHOUT Numbers\n";
+			s.clear();
+			return AskAndChackIsDigit(); /*Рекурсивно викликаємо функцію
+										   AskAndChackIsDigit до того моменту
+										   поки користувач не введе string без чисел*/
 		}
 	}
-
-	if (IsAlphabetic == false)
-		std::cout << "String in NOT in alphabetical sequence";
-	else
-		std::cout << "String in alphabetical sequence";
-
-	return 0;
+	return s;
 }
